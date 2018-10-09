569a8367ca2977f44a314942224fcf65fcea14ba
VFrancishko <itmymaill@gmail.com>
2018-10-09 15:18:10 +0300

    new changes

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,20 +1,19 @@
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
-import 'package:angular_app/src/hero.dart';
 import 'src/hero.dart';
+import 'src/hero_component.dart';
 import 'src/mock_heroes.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
-  directives: [formDirectives,coreDirectives],
   styleUrls: ['app_component.css'],
+  directives: [coreDirectives,HeroComponent],
 )
+
 class AppComponent {
   var name = 'Slava';
   final title = 'Tour of herous';
   List<Hero> heroList = mockHeroes;
   Hero selectedHero;
-
   void onSelect(Hero hero) => selectedHero = hero;
 }
