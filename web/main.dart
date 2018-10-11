c9ddbbe89da284ec29c1348dcebc50f3e4e844b3
VFrancishko <itmymaill@gmail.com>
2018-10-11 18:27:00 +0300

    http implementation

--- web/main.dart
+++ web/main.dart
@@ -1,16 +1,16 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:angular_app/app_component.template.dart' as ng;
+import 'package:angular_app/in_memory_data_service.dart';
+import 'package:http/http.dart';
 
 import 'main.template.dart' as self;
 
-@GenerateInjector(
+@GenerateInjector([
   routerProvidersHash,
-)
-//final InjectorFactory injector = self.injector$Injector;
+  ClassProvider(Client, useClass: InMemoryDataService),
+])
 final InjectorFactory injector = self.injector$Injector;
-
-
 
 void main() {
   runApp(ng.AppComponentNgFactory, createInjector: injector);
